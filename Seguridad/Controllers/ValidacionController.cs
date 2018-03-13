98f7f74601fa321a5ecfcc9bfd62e2ba5edfabdc
PabloBarrantes <pabh92@gmail.com>
2018-03-13 18:18:05 +0100

    Initial commit
    Proyecto como tarea de clase de Tajamar en el curso de MCSD 2018

---
+++ Seguridad/Controllers/ValidacionController.cs
@@ -0,0 +1,44 @@
+﻿using Seguridad.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Seguridad.Controllers
+{
+    public class ValidacionController : Controller
+    {
+        // GET: Validacion
+        public ActionResult Acceso()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(String usuario, String pass)
+        {
+            ModeloUsuarios modelo = new ModeloUsuarios();
+            USUARIOS user = modelo.ExisteUsuario(usuario, pass);
+            if (user != null)
+            {
+                FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, user.userName, DateTime.Now, DateTime.Now.AddHours(1), true, "", FormsAuthentication.FormsCookiePath);
+                String datosCifrados = FormsAuthentication.Encrypt(ticket);
+                HttpCookie cookie = new HttpCookie("cookieUsuario", datosCifrados);
+                Response.Cookies.Add(cookie);                
+                return RedirectToAction("Acceso", "Validacion");
+            }
+            else
+            {
+                ViewBag.Mensaje = "Usuario/Contraseña incorrecta!";
+            }
+            return View();
+        }
+    }
+}
---
+++ Seguridad/Models/ModeloUsuarios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Seguridad.Models
+{
+    public class ModeloUsuarios
+    {
+        ContextoSeguridad contexto;
+
+        public ModeloUsuarios()
+        {
+            contexto = new ContextoSeguridad();
+        }
+
+        public USUARIOS ExisteUsuario(String username, String pass)
+        {
+            var consulta = from datos in contexto.USUARIOS
+                           where datos.userName == username
+                           && datos.pass == pass
+                           select datos;
+            return consulta.FirstOrDefault();
+        }
+
+    }
+}
